1d2c6ec75983735b9295e2659c3733e42e7a25fa
Glaydson <glaydsonvs@gmail.com>
2020-09-03 19:59:57 -0300

    Inclusao de linha

--- src/unidade3/AloMundo.java
+++ src/unidade3/AloMundo.java
@@ -5,6 +5,7 @@ public class AloMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Olá Mundo...");
+		System.out.println("Hoje é quinta...");
 	}
 
 }
