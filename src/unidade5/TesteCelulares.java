06fa5bb03d6b46d3796882aef09506d821a29ad4
Glaydson <glaydsonvs@gmail.com>
2020-09-22 20:07:16 -0300

    Aula de 22-09-2020

--- src/unidade5/TesteCelulares.java
+++ src/unidade5/TesteCelulares.java
@@ -4,10 +4,20 @@ public class TesteCelulares {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Celular samsung = new Celular("Samsung", "S14", 128);
+//		Celular samsung = new Celular("Samsung", "S14", 128);
 
-		System.out.println(samsung.getMarca());
-		System.out.println(samsung);
+//		System.out.println(samsung.getMarca());
+//		System.out.println(samsung);
+		
+		int memoriaInterna = 128;
+		Celular iphone7 = new Celular (
+			"Iphone", "7plus", memoriaInterna);
+		memoriaInterna = 256;
+		Celular samsung = new Celular(
+			"Samsung", "Galaxy", iphone7.getTamanhoMemoria());
+		System.out.println(
+			samsung.getTamanhoMemoria());
+
 	}
 
 }
---
+++ src/unidade5/TesteTempo.java
@@ -0,0 +1,16 @@
+package unidade5;
+
+public class TesteTempo {
+
+	public static void main(String[] args) {
+		
+		Tempo tempo1 = new Tempo(10);
+		Tempo tempo2 = new Tempo(10, 30);
+		Tempo tempo3 = new Tempo(10, 30, 10);
+		
+		System.out.println(tempo1);
+		System.out.println(tempo2);
+		System.out.println(tempo3);
+	}
+
+}
